88c5706dd45d8fa5f811dd4fb23be251231956e7
victormrtns <vmhugo00@gmail.com>
2022-09-12 21:47:52 -0300

    Injetando valores literais via xml

--- questao1-curso/src/main/java/com/luv2code/springdemo/CricketCoach.java
+++ questao1-curso/src/main/java/com/luv2code/springdemo/CricketCoach.java
@@ -1,7 +1,29 @@
 package com.luv2code.springdemo;
 
 public class CricketCoach implements Coach{
+
+    private String emailAddress;
+   private String team;
+
+
     private FortuneService fortuneService;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
     //create a no arg constructor
     public CricketCoach() {
         System.out.println("Cricket Coach: inside no-arg constructor");
